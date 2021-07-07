b5768967694461c0371b4da29a274cca3ede4a33
xCarlosDevx <78973956+xCarlosDevx@users.noreply.github.com>
2021-07-07 16:55:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3050;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -22,68 +22,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
 
-// all customers
-app.get('/customers', (req, res) => {
-  const sql = 'SELECT * FROM empleados';
-
-  connection.query(sql, (error, results) => {
-    if (error) throw error;
-    if (results.length > 0) {
-      res.json(results);
-    } else {
-      res.send('Not result');
-    }
-  });
-});
-
-app.get('/customers/:id', (req, res) => {
-  const { id } = req.params;
-  const sql = `SELECT * FROM customers WHERE id = ${id}`;
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-
-    if (result.length > 0) {
-      res.json(result);
-    } else {
-      res.send('Not result');
-    }
-  });
-});
-
-app.post('/add', (req, res) => {
-  const sql = 'INSERT INTO customers SET ?';
-
-  const customerObj = {
-    name: req.body.name,
-    city: req.body.city
-  };
-
-  connection.query(sql, customerObj, error => {
-    if (error) throw error;
-    res.send('Customer created!');
-  });
-});
-
-app.put('/update/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, city } = req.body;
-  const sql = `UPDATE customers SET name = '${name}', city='${city}' WHERE id =${id}`;
-
-  connection.query(sql, error => {
-    if (error) throw error;
-    res.send('Customer updated!');
-  });
-});
-
-app.delete('/delete/:id', (req, res) => {
-  const { id } = req.params;
-  const sql = `DELETE FROM customers WHERE id= ${id}`;
-
-  connection.query(sql, error => {
-    if (error) throw error;
-    res.send('Delete customer');
-  });
-});
 
 // Check connect
 connection.connect(error => {
